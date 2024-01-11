0c3879505d001a5e68db0836ba92a7d7ab1ab6be
RavaszJan <156021333+RavaszJan@users.noreply.github.com>
2024-01-11 14:41:38 +0100

    doplneny jednoriadkovy print

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ class City:
         self.numberofstreet = cislodomu
         self.zip = smerovecislo
         self.country = stat
-    def adress(self):
-        print("Adress:")
+    def address(self):
+        print("Address:")
         print(f"city:{self.city}")
         print(f"street:{self.street}")
         print(f"number of street:{self.numberofstreet}")
         print(f"zip:{self.zip}")
         print(f"country:{self.country}")
         print()
+        print(f"Address:\n street:{self.city} \n number of street:{self.numberofstreet} \n zip:{self.zip} \n country:{self.country}")
 
 adresa1=City("Bratislava","Moja",1,85320,"Slovensko")
-adresa1.adress()
+adresa1.address()
 adresa2=City("Malacky","Bratislavska",5,90001,"SR")
-adresa2.adress()
+adresa2.address()
 adresa3=City("Martin","Maticna",18,70023,"Slovenska republika")
-adresa3.adress()
+adresa3.address()
 
